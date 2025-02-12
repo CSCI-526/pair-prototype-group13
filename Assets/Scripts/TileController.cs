8b2870f8d25d810584ccabb312724c265b610365
Nithin Aditya <nithinaditya@gmail.com>
2025-02-11 21:58:36 -0800

    chip placement and flipping mechanics

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class TileController : MonoBehaviour
 {
-    // Start is called before the first frame update
     private GameController gameController;
     private int x, y;
     private bool hasShip;
+    public GameObject chipPrefab; 
+
+    private GameObject placedChip = null; //track placed chip
+    private static bool isBlackTurn = true;  //track turns
+
+    private static Dictionary<Vector2Int, TileController> tileMap = new Dictionary<Vector2Int, TileController>(); // Stores all tiles
 
     public void Init(GameController controller, int xCoord, int yCoord, bool shipPresent)
     {
@@ -15,29 +20,76 @@ public class TileController : MonoBehaviour
         x = xCoord;
         y = yCoord;
         hasShip = shipPresent;
-    }
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        tileMap[new Vector2Int(x, y)] = this; 
     }
 
     private void OnMouseDown()
     {
         Debug.Log($"Tile clicked at ({x}, {y}) - Ship Present: {hasShip}");
+
+        
         if (hasShip)
         {
-            GetComponent<SpriteRenderer>().color = Color.red; // Change color if ship is present
+            GetComponent<SpriteRenderer>().color = Color.red; //change tile color if  ship is present
         }
-        else
+
+        //place chip only if one isn't already placed
+        if (placedChip == null && chipPrefab != null)
         {
-            GetComponent<SpriteRenderer>().color = Color.blue; // Change color if empty
+            Vector3 chipPosition = transform.position + new Vector3(0, 0, -1f); 
+            placedChip = Instantiate(chipPrefab, chipPosition, Quaternion.identity);
+            placedChip.transform.SetParent(transform); //attach chip to tile
+            placedChip.transform.localScale = new Vector3(0.8f, 0.8f, 1);
+
+            //change chip color based on turn
+            SpriteRenderer chipRenderer = placedChip.GetComponent<SpriteRenderer>();
+            if (chipRenderer != null)
+            {
+                chipRenderer.color = isBlackTurn ? Color.black : Color.white; 
+            }
+
+            FlipOpponentChips(isBlackTurn);
+            isBlackTurn = !isBlackTurn; //switch turn
+        }
+    }
+    //flipping mechanics
+    private void FlipOpponentChips(bool isBlack)
+    {
+        Vector2Int[] directions = {
+            new Vector2Int(1, 0), new Vector2Int(-1, 0), //horizontal
+            new Vector2Int(0, 1), new Vector2Int(0, -1), //vertical
+            new Vector2Int(1, 1), new Vector2Int(-1, -1), //diagonal - \
+            new Vector2Int(1, -1), new Vector2Int(-1, 1) //diagonal - /
+        };
+
+        foreach (var dir in directions)
+        {
+            List<TileController> toFlip = new List<TileController>();
+            Vector2Int checkPos = new Vector2Int(x, y) + dir;
+
+            while (tileMap.ContainsKey(checkPos) && tileMap[checkPos].placedChip != null)
+            {
+                SpriteRenderer chipRenderer = tileMap[checkPos].placedChip.GetComponent<SpriteRenderer>();
+                if (chipRenderer == null) break;
+
+                Color chipColor = chipRenderer.color;
+                Color currentTurnColor = isBlack ? Color.black : Color.white;
+
+                if (chipColor == currentTurnColor) //flip all stored if a matching color is found
+                {
+                    foreach (var tile in toFlip)
+                    {
+                        tile.placedChip.GetComponent<SpriteRenderer>().color = currentTurnColor;
+                    }
+                    break;
+                }
+                else
+                {
+                    toFlip.Add(tileMap[checkPos]); 
+                }
+
+                checkPos += dir;
+            }
         }
     }
 }
